822ea401781e1d76579291f50a0cc584bb6085f3
rakeshpawar@7848 <rakesh.pawar@exathought>
2024-09-30 15:07:35 +0530

    rakesh pawar first commit

--- ADVANCE_SELENIUM/src/test/java/practise/testng/dataprovider/RakeshTest.java
+++ ADVANCE_SELENIUM/src/test/java/practise/testng/dataprovider/RakeshTest.java
@@ -6,12 +6,7 @@ public class RakeshTest {
 	@Test
 	public void addingTest()
 	{
-		System.out.println(" rakesh test createf for push purp[oses new file created");
-		System.out.println(" adding 2 nd merge conflicit line to check merge conflict");
-		//adding lines
-		
-		System.out.println(" adding merge conflict 3");
-		System.out.println(" Rakesh2 merge conflicit");
+		System.out.println(" rakesh pawar");
 	}
 
 }
