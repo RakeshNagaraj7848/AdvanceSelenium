030e03176c7755202a53789dd63eb53b8fbd6479
rakeshpawar@7848 <rakesh.pawar@exathought>
2024-09-30 12:24:22 +0530

    added a new file

---
+++ ADVANCE_SELENIUM/src/test/java/practise/testng/dataprovider/RakeshTest.java
@@ -0,0 +1,12 @@
+package practise.testng.dataprovider;
+
+import org.testng.annotations.Test;
+
+public class RakeshTest {
+	@Test
+	public void addingTest()
+	{
+		System.out.println(" rakesh test createf for push purp[oses new file created");
+	}
+
+}
